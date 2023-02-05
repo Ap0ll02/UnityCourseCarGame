31e085701f1f2f1cb949e1ae4aa642ff0048829c
Apollo <jackratermann@gmail.com>
2023-02-05 15:54:41 -0800

    Epic Stuff

--- MyEpicGame/Assets/driver.cs
+++ MyEpicGame/Assets/driver.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class driver : MonoBehaviour
 {
+  [SerializeField] float steerSpeed = 0.1f;
+  [SerializeField] float moveSpeed = 0.005f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +15,10 @@ public class driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      transform.Rotate(0,0,0.1f);
-      transform.Translate(0.05f,0,0);
+      float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+      float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+      transform.Rotate(0,0,-steerAmount);
+      transform.Translate(0,moveAmount,0);
       
     }
 }
