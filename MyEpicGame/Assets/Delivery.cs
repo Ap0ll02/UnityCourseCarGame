e7ca15f8a85a37f46afa9b2bbb7d11e114fa7d89
Apollo <jackratermann@gmail.com>
2023-02-28 17:30:23 -0800

    ExtraUpdates
    Start of Destruction of Packages when picked up.

--- MyEpicGame/Assets/Delivery.cs
+++ MyEpicGame/Assets/Delivery.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
    bool hasPackage = false;
+
+   [SerializeField] float delayDestroy = 0.5f;
+   
    void OnTriggerEnter2D(Collider2D other)
    {
         if(other.tag == "Package"){
          hasPackage = true;
+         Destroy(other, delayDestroy);
         }
         else if(other.tag == "Customer" && hasPackage){
             Debug.Log("Package Delivered");
